41b878f24bb9041324a1faa7b2f2b06d1f34616f
joyusjunior <joyusjunior@gmail.com>
2025-04-29 15:42:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,7 @@ from app import app
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+app.secret_key = os.environ.get('SECRET_KEY') or 'dev'  # Must be set properly
+app.config['SESSION_COOKIE_SECURE'] = True  # Required for HTTPS
+app.config['SESSION_COOKIE_HTTPONLY'] = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'Lax'
